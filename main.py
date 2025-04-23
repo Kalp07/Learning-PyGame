aef56969d6189adc0807b9dd6f47a3f1cfa0f6ee
Kalp07 <joshikalp111@gmail.com>
2025-04-23 21:32:18 +0530

    Added random stars

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
+from os.path import join
+import random
 
 pygame.init()
-
-screen = pygame.display.set_mode((800, 600))
-
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('assets/ufo.png')
-pygame.display.set_icon(icon)
-
 running = True
+
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+player_surf = pygame.image.load(join('assets', 'images', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('assets', 'images', 'star.png')).convert_alpha()
+star_positions = [(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    display_surface.fill('darkgrey')
+    x += 0.1
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
+    display_surface.blit(player_surf, (x, 150))
     pygame.display.update()
+
+pygame.quit()
